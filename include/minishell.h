e1258e65e07a9dbe53652364c8c8cee7aedf765e
Pascal Hurni <phi@pragmaticpurist.org>
2024-01-24 11:27:19 +0100

    Strip minishell.h to strict minimum

--- include/minishell.h
+++ include/minishell.h
@@ -13,78 +13,45 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include "libft.h"
-# include <fcntl.h>
-# include <limits.h>
-# include <pthread.h>
-# include <stdarg.h>
-# include <stdbool.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <sys/time.h>
 # include <unistd.h>
-# include <readline/readline.h>
-# include <readline/history.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "libft.h"
+# include "alist.h"
+# include "dstr.h"
 
-typedef struct s_cmd	s_cmd;
-
-typedef void			(*t_builtin)(char **, int, int, char **);
+typedef void	(*t_builtin)(t_alist *args, int, int, char **);
 
 typedef struct s_cmd
 {
-	char				*path;
-	char				*cmd;
-	char				**args;
-	t_builtin			builtin;
-	char				*dirin;
-	char				*dirout;
-	s_cmd				*next;
-}						t_cmd;
+	t_anode		node;
+	char		*path;
+	t_builtin	builtin;
+	t_alist		args;
+	char		*redir_in_name;
+	char		*redir_out_name;
+}	t_cmd;
 
-typedef struct s_builtin_map
+typedef struct s_arg
 {
-	char				*name;
-	t_builtin			func;
-}						t_builtin_map;
+	t_anode		node;
+	t_dstr		string;
+}	t_arg;
 
-// CLEAN.C
-void					free_cmd_list(t_cmd *cmd_lst);
-void					free_tab(char **tab);
+t_arg	*arg_init(size_t size);
+t_cmd	*cmd_init(void);
 
-// PATH.C
-void					programm_path(t_cmd *lst);
-bool					is_programm(t_cmd *lst);
-char					*get_path(t_cmd *list);
-void					check_path(t_cmd *list);
-
-// PARSING.C
-void					add_redirection(char **cmd_split, int *j, t_cmd *node);
-t_cmd					*init_cmd(char **split);
-void					parsing(const char *input, char **env);
-
-// LST_UTILS.C
-void					update_cmd_with_builtin(t_cmd *node);
-t_cmd					*init_node_variables(t_cmd *node);
-t_cmd					*create_new_cmd_node(char **cmd_split);
-t_cmd					*cmd_lastlst(t_cmd *lst);
-void					cmd_lstadd_back(t_cmd **lst, t_cmd *new);
-
-// UTILS.C
-void					print_command_details(t_cmd *cmd, int count);
-void					print_cmd(t_cmd *cmd_lst);
-void					init_map(t_builtin_map *builtin_map);
-void    				print_args(char **args);
+int		parse_commands(t_alist *commands, char *input);
+int		run_commands(t_alist *commands, char **env);
+int		free_commands(t_alist *commands, int retval);
 
 // builtin
-void					builtin_echo(char **args, int in_fd, int out_fd, char **env);
-void					builtin_cd(char **args, int in_fd, int out_fd, char **env);
-void					builtin_pwd(char **args, int in_fd, int out_fd, char **env);
-void					builtin_export(char **args, int in_fd, int out_fd, char **env);
-void					builtin_unset(char **args, int in_fd, int out_fd, char **env);
-void					builtin_env(char **args, int in_fd, int out_fd, char **env);
-void					builtin_exit(char **args, int in_fd, int out_fd, char **env);
-
-void    run_commands(t_cmd *commands, char **env);
+void	builtin_echo(char **args, int in_fd, int out_fd, char **env);
+void	builtin_cd(char **args, int in_fd, int out_fd, char **env);
+void	builtin_pwd(char **args, int in_fd, int out_fd, char **env);
+void	builtin_export(char **args, int in_fd, int out_fd, char **env);
+void	builtin_unset(char **args, int in_fd, int out_fd, char **env);
+void	builtin_env(char **args, int in_fd, int out_fd, char **env);
+void	builtin_exit(char **args, int in_fd, int out_fd, char **env);
 
 #endif
