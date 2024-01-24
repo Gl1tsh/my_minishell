f0a334c8133b230c6e35b72274b65de2b39c9035
Powlar <powlarofficial@gmail.com>
2024-01-24 16:00:11 +0100

    header fixed

--- src/parsing.c
+++ src/parsing.c
@@ -1,32 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cd.c                                               :+:      :+:    :+:   */
+/*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nagiorgi <nagiorgi@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/23 17:22:10 by nagiorgi          #+#    #+#             */
-/*   Updated: 2024/01/23 17:28:36 by nagiorgi         ###   ########.fr       */
+/*   Created: 2024/01/24 15:47:19 by nagiorgi          #+#    #+#             */
+/*   Updated: 2024/01/24 15:56:24 by nagiorgi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	builtin_cd(char **args, int in_fd, int out_fd, char **env)
-{
-	char	**path;
-
-	if (args[1] == NULL)
-		chdir(getenv("HOME"));
-	else
-	{
-		if (args[1][0] == '~' && args[1][1] != '\0')
-		{
-			chdir(getenv("HOME"));
-		}
-		else if (args[1][0] == '~')
-			chdir(ft_strjoin(getenv("HOME"), args[1] + 1));
-		else
-			chdir(args[1]);
-	}
-}
