d61f601c2af9f286f019188e2a230f01f921227e
Pascal Hurni <phi@pragmaticpurist.org>
2024-01-25 16:27:14 +0100

    Enhance print_commands debug and add free_commands

--- src/main.c
+++ src/main.c
@@ -19,21 +19,66 @@ void print_commands(t_cmd *cmds)
 {
 	t_arg	*arg;
 
-	fprintf(stderr, "cmd : args %p \n", cmds->args);
-	arg = cmds->args;
-	while (arg != NULL)
+	while (cmds != NULL)
 	{
-		fprintf(stderr, "arg : %p \n", arg);
-		fprintf(stderr, "arg : [%s]\n", arg->dynamic_str.bytes);
-		arg = arg->next;
+		fprintf(stderr, "\ncmd:\n");
+		fprintf(stderr, "  args: %p \n", cmds->args);
+		arg = cmds->args;
+		while (arg != NULL)
+		{
+			fprintf(stderr, "  arg: %p [%s]\n", arg, arg ? arg->dynamic_str.bytes : "");
+			arg = arg->next;
+		}
+		cmds = cmds->next;
 	}
+}
+
+int free_commands(t_cmd *cmd, int retval)
+{
+	t_arg	*arg;
+	t_arg	*next_arg;
+	t_cmd	*next_cmd;
+
+	if (retval)
+		printf("Error %d\n", retval);
+
+	while (cmd != NULL)
+	{
+		arg = cmd->args;
+		while (arg != NULL)
+		{
+			next_arg = arg->next;
+			dstr_free(&arg->dynamic_str);
+			free(arg);
+			arg = next_arg;
+		}
+		next_cmd = cmd->next;
+		free(cmd);
+		cmd = next_cmd;
+	}
+
+	return retval;
+}
+
+int	process(char *input, char **env)
+{
+	t_cmd	*cmds;
+	int		retval;
+
+	retval = parse_commands(&cmds, input, env);
+	if (retval)
+		return free_commands(cmds, retval);
+
+	//retval = run_commands(cmds, env);
+	print_commands(cmds);
+	return free_commands(cmds, retval);
 }
 
 int	main(int argc, char **argv, char **env)
 {
 	char	*input;
+	char	*trimmed_input;
 	char	*prompt;
-	t_cmd	*cmds;
 
 	(void)argc;
 	(void)argv;
@@ -41,15 +86,16 @@ int	main(int argc, char **argv, char **env)
 	{
 		prompt = ft_strjoin(getenv("PWD"), " > ");
 		input = readline(prompt);
+		free(prompt);
 		if (!input)
 			break ;
-		if (*input)
+		trimmed_input = ft_strtrim(input, " \t");
+		if (*trimmed_input)
 		{
-			add_history(input);
-			parse_commands(&cmds, input, env);
-			//run_commands(cmds, env);
-			print_commands(cmds);
+			add_history(trimmed_input);
+			process(trimmed_input, env);
 		}
+		free(trimmed_input);
 		free(input);
 	}
 	return (0);
