111f65e58cb7440f1c8d8ab9c36d9f13af103dbc
Pascal Hurni <phi@pragmaticpurist.org>
2024-02-02 23:08:18 +0100

    Add error handling and normine heredoc.c

--- src/heredoc.c
+++ src/heredoc.c
@@ -20,7 +20,7 @@ void	heredoc_signal_handler(int sig_num)
 	close(0);
 }
 
-char	*generate_pid_string(char *target)
+char	*pid_to_str(char *target)
 {
 	int	pid;
 	int	i;
@@ -34,6 +34,24 @@ char	*generate_pid_string(char *target)
 	}
 	target[8] = 0;
 	return (target);
+}
+
+int	write_textfile(char *pathname, char *content)
+{
+	int	fd;
+	int	bytes_written;
+	int	len;
+
+	len = ft_strlen(content);
+	fd = open(pathname, O_CREAT | O_TRUNC | O_WRONLY, 0777);
+	if (fd < 0)
+		return (perror_return(NULL, 1));
+	bytes_written = write(fd, content, len);
+	if (close(fd) != 0)
+		return (perror_return(NULL, 1));
+	if (bytes_written != len)
+		return (perror_return(NULL, 1));
+	return (0);
 }
 
 int	read_heredoc(t_dstr *heredoc, char *eos)
@@ -67,9 +85,10 @@ int	process_heredoc(t_cmd *cmds)
 	char	temp_filename[9];
 	char	*temp_pathname;
 	char	*temp_dirname;
-	int		fd;
 	t_dstr	heredoc;
+	int		exit_status;
 
+	exit_status = 0;
 	if (cmds->dirin != NULL && cmds->dirin_mode == DIRIN_MODE_HEREDOC)
 	{
 		dstr_allocate(&heredoc, 128);
@@ -81,13 +100,11 @@ int	process_heredoc(t_cmd *cmds)
 		temp_dirname = getenv("TMPDIR");
 		if (temp_dirname == NULL)
 			temp_dirname = "/tmp";
-		temp_pathname = join_path(temp_dirname, generate_pid_string(temp_filename));
-		fd = open(temp_pathname, O_CREAT | O_TRUNC | O_WRONLY, 0777);
-		write(fd, heredoc.bytes, ft_strlen(heredoc.bytes));
-		close(fd);
+		temp_pathname = join_path(temp_dirname, pid_to_str(temp_filename));
+		exit_status = write_textfile(temp_pathname, heredoc.bytes);
 		free(cmds->dirin);
 		dstr_free(&heredoc);
 		cmds->dirin = temp_pathname;
 	}
-	return (0);
+	return (exit_status);
 }
