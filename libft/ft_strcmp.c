6eaaf445372b7e7c20241e6f167faec02cc3c6a0
Powlar <powlarofficial@gmail.com>
2024-02-05 16:45:08 +0100

    add fonction ft_strcmp libft

---
+++ libft/ft_strcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nagiorgi <nagiorgi@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 16:17:59 by nagiorgi          #+#    #+#             */
+/*   Updated: 2024/02/05 16:23:17 by nagiorgi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	if (s1 == NULL && s2 == NULL)
+		return (0);
+	if (s1 == NULL)
+		return (-1);
+	if (s2 == NULL)
+		return (1);
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -107,6 +107,7 @@ char	*ft_strdup(const char *src);
 char	*ft_strndup(const char *src, size_t n);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+int		ft_strcmp(char *s1, char *s2);
 int		ft_strncmp(const char *s1, char *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
