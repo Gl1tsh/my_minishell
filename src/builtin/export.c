87dace3f803d3629425469edbc41fddb2208e219
Powlar <powlarofficial@gmail.com>
2024-02-21 14:09:43 +0100

    export print env

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: nagiorgi <nagiorgi@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 09:10:29 by nagiorgi          #+#    #+#             */
-/*   Updated: 2024/02/10 09:10:32 by nagiorgi         ###   ########.fr       */
+/*   Updated: 2024/02/21 14:07:22 by nagiorgi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 int	builtin_export(char **args, t_env *env)
 {
 	args++;
+	if (*args == NULL)
+		return (builtin_env(args, env));
 	while (*args)
 	{
 		if (!is_valid_var_name(*args))
